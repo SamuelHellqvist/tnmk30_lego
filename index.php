a30f8b7f3837e099e9520f803cb84feb30b07c64
davemm22 <davidsson470@gmail.com>
2022-11-22 18:20:38 +0100

    Co-authored-by: SamuelHellqvist <SamuelHellqvist@users.noreply.github.com>

--- index.php
+++ index.php
@@ -13,21 +13,26 @@
 <body>
     <!-- header -->
     <header> 
-
-        <img class="left" src="images/image.jpg" alt="logo">
-        <h1 class ="left"> Name </h1>
-
+        <section>
+            <img class="left" id="logo_header" src="images/image.jpg" alt="logo">
+            <h1 class ="left"> Name </h1>
+        </section>
+        
+        <section id="header_section2">
         <button id="how_to_use_btn">How to use</button>
-
+        </section>
+        <section>hi</section>
     </header>
     <div class="search_section">
         <img class="left" src="images/image.jpg" alt="logo">
         <form>
-        <input type="text" id="search_bar">
+        <input type="text" id="search_bar" placeholder="Find your inner piece...">
         </form> 
     </div>
-    <?php 
-
-    ?>
+    
+    <footer>
+        <button class="footer_btn" id="about">about</button>
+        <button class="footer_btn" id="cookies">cookies</button>
+    </footer>
 </body>
 </html>
