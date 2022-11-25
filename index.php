3c59f35574d8a8386dbc065cb841c6234493021b
QueueSecret <kriho388@student.liu.se>
2022-11-25 10:49:29 +0100

    Ändrat med CSS och Header i index

--- index.php
+++ index.php
@@ -6,33 +6,29 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Huvudsida</title>
     <link rel="stylesheet" href="style.css">
     <script src="script.js" defer></script>
 </head>
 <body>
     <!-- header -->
     <header> 
-        <section>
-            <img class="left" id="logo_header" src="images/image.jpg" alt="logo">
-            <h1 class ="left"> Name </h1>
-        </section>
+        <img id="logo_header" src="images/image.jpg" alt="logo">
         
-        <section id="header_section2">
-        <button id="how_to_use_btn">How to use</button>
-        </section>
-        <section>hi</section>
+        <div><button>How to use</button></div>
+
     </header>
+
     <div class="search_section">
-        <img class="left" src="images/image.jpg" alt="logo">
+        <img src="images/image.jpg" alt="logo">
         <form>
         <input type="text" id="search_bar" placeholder="Find your inner piece...">
         </form> 
     </div>
     
     <footer>
-        <button class="footer_btn" id="about">about</button>
-        <button class="footer_btn" id="cookies">cookies</button>
+        <button id="about">about</button>
+        <button id="cookies">cookies</button>
     </footer>
 </body>
 </html>
