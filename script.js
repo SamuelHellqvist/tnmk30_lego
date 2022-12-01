405bbf716ad5824e3a473efbf80b02915d3a7d88
SamuelHellqvist <samuel@hellqvist.nu>
2022-12-01 16:41:17 +0100

    adding the to top button

--- script.js
+++ script.js
@@ -23,3 +23,30 @@ function popupFunction(id){
         popup.style.display = "block";
       }
 }
+
+
+
+
+/*Samuels lilla script*/
+
+/* This scirpt originates from w3schools */
+let topbutton = document.getElementById("topBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+    topbutton.style.display = "block";
+  } else {
+    topbutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+topbutton.addEventListener("click", topFunction);
