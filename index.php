6cf65a8b0c015e7df70d4263d197538e111adeff
SamuelHellqvist <samuel@hellqvist.nu>
2022-12-09 14:33:37 +0100

    Ändrat css i visade poster

--- index.php
+++ index.php
@@ -68,7 +68,7 @@
     <div class="search_section">
         <img id="search_image" src="images/image.jpg" alt="logo">
         <form action="search.php" method="get" >
-        <input type="text" id="search_bar" placeholder="Find your inner piece...">
+            <input type="text" id="search_bar" placeholder="Find your inner piece...">
         </form> 
     </div>
     
