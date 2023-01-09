d90a1ce3d934e0d17f87ee05fb3d60b7eabba610
Majsburken <annabark1@gmail.com>
2023-01-09 15:29:00 +0100

    Sorterat Css:en, Kommenterat index.php

--- index.php
+++ index.php
@@ -1,75 +1,74 @@
 <!-- php-dokumentet för webbsidans "homepage" -->
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Huvudsida</title>
-    <link rel="stylesheet" href="style.css">
-    <script src="script.js" defer></script>
-</head>
-<body>
-    <img id="brick_left" src="images/bricksideLeft.png" alt="bricks left">
-    <img id="brick_right" src="images/bricksideRight.png" alt="bricks right">
 
-    <!-- header -->
-    <header> 
-    <a href="index.php"><img id="logo_header" src="images/logoHeader.png" alt="logo"></a>
-        
-        <div class="popup" onclick="popupFunction(howToUsePopup)"> 
-        <button id="how_to_use_btn">How to Use</button>
-        <button id="how_to_use_btn_min">?</button>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Huvudsida</title>
+        <link rel="stylesheet" href="style.css">
+        <script src="script.js" defer></script>
+    </head>
 
-        <!-- <button id="?_btn">?</button> -->
-        </div>
-        <article class="popuptext" id="howToUsePopup">
-            <button onclick="closePopup(howToUsePopup)" class="closeBtn">+</button>
-            <?php include("howToUse.txt"); ?>
-        </article>
+    <body>
+        <!-- Bakgrundsbilder -->
+        <img id="brick_left" src="images/bricksideLeft.png" alt="bricks left">
+        <img id="brick_right" src="images/bricksideRight.png" alt="bricks right">
 
-        <article class="popuptext" id="aboutUsPopup">
-            <button onclick="closePopup(aboutUsPopup)" class="closeBtn">+</button>
-            <?php include("aboutUs.txt"); ?>
-        </article>
+        <!-- header -->
+        <header> 
+            <a href="index.php"><img id="logo_header" src="images/logoHeader.png" alt="logo"></a>
 
-        <article class="popuptext" id="cookiesPopup">
-            <button onclick="closePopup(cookiesPopup)" class="closeBtn">+</button>
-            <?php include("cookies.txt"); ?>
-        </article>
+            <div class="popup" onclick="popupFunction(howToUsePopup)"> 
+                <button id="how_to_use_btn">How to Use</button>
+                <button id="how_to_use_btn_min">?</button>
+            </div>
 
-    </header>
+            <article class="popuptext" id="howToUsePopup">
+                <button onclick="closePopup(howToUsePopup)" class="closeBtn">+</button>
+                <?php include("howToUse.txt"); ?>
+            </article>
 
-    <?php
-    session_start();
-    ?>
+            <article class="popuptext" id="aboutUsPopup">
+                <button onclick="closePopup(aboutUsPopup)" class="closeBtn">+</button>
+                <?php include("aboutUs.txt"); ?>
+            </article>
 
-    <div class="search_section">
-        <img id="search_image" src="images/logoHomepage.png" alt="logo">
+            <article class="popuptext" id="cookiesPopup">
+                <button onclick="closePopup(cookiesPopup)" class="closeBtn">+</button>
+                <?php include("cookies.txt"); ?>
+            </article>
+        </header>
+
+        <?php
+        session_start();
+        ?>
+
+        <div class="search_section">
+            <img id="search_image" src="images/logoHomepage.png" alt="logo">
+
             <form action="search.php" method="get" >
-            <input type="text" name="searchResult" id="search_bar" placeholder="Find your inner piece...">
+                <input type="text" name="searchResult" id="search_bar" placeholder="Find your inner piece...">
 
-            <input type="image" id="searchBtn" onmouseover="this.src='images/magnifyingGlassHover.png'" 
-             alt="magnifying glass" onmouseout="this.src='images/magnifyingGlass.png'" src="images/magnifyingGlass.png">
-            <!-- <input type="image" id="searchBtnHover" src="images/magnifyingGlassHover.png" alt="Magnifying Glass hover"> -->
+                <input type="image" id="searchBtn" onmouseover="this.src='images/magnifyingGlassHover.png'" 
+                alt="magnifying glass" onmouseout="this.src='images/magnifyingGlass.png'" src="images/magnifyingGlass.png">
             </form> 
-    </div>
+        </div>
 
-    
-
-    
-    <footer>
-    <div class="popup" onclick="popupFunction(aboutUsPopup)"> 
-    <button id="about" >About us</button>
-    
-    </div>
-    <div class="popup" onclick="popupFunction(cookiesPopup)"> 
-    <button id="cookies" >Cookies</button>
+        <footer>
+            <div class="popup" onclick="popupFunction(aboutUsPopup)"> 
+                <button id="about" >About us</button>
             
-    </div>    
-    </footer>
-</body>
+            </div>
+            <div class="popup" onclick="popupFunction(cookiesPopup)"> 
+                <button id="cookies" >Cookies</button>
+                    
+            </div>    
+        </footer>
+    </body>
 </html>
+
 <?php
     session_unset();
 ?>
